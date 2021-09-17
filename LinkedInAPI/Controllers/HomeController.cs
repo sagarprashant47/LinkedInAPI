e7a90e13f524e14eb443bd0420d49fce47dac0a3
Prashant Sagar <prashant.sagar@collabera.com>
2021-09-17 23:16:05 +0530

    Add project files.

---
+++ LinkedInAPI/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using LinkedInAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedInAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public static List<Post> posts = new List<Post>();
+        public static List<Comment> comments = new List<Comment>();
+        public static List<Like> likes = new List<Like>();
+
+        [HttpGet]
+        public List<Post> Get()
+        {
+
+            return posts;
+            //return new Post[] {
+            //    new Post(){Comments="coments", Likes=1 ,Text ="text" , Id=1,PostDateTime=DateTime.Now} 
+            //};
+        }
+
+        [HttpPost]
+        [Route("UpdatePost")]
+        public void UpdatePost(Post model)
+        {
+            model.Id= posts.Count + 1;
+            model.PostDateTime = DateTime.UtcNow;
+            posts.Add(model);
+
+        }
+
+        [HttpPost]
+        [Route("AddComment/{postId}")]
+        public Post AddComment(int postId, Comment model)
+        {
+            model.CommentDateTime = DateTime.UtcNow;
+            model.Id = comments.Count + 1;
+            model.PostId = postId;
+            comments.Add(model);
+            posts.Where(x => x.Id == postId).FirstOrDefault().PostComments.Add(model);
+            return posts.Where(x => x.Id == postId).FirstOrDefault();
+        }
+
+        [HttpGet]
+        [Route("AddLike/{postId}")]
+        public Post AddLike(int postId)
+        {
+            Like model = new Like();
+            model.LikeDateTime = DateTime.UtcNow;
+            model.Id = likes.Count + 1;
+            model.PostId = postId;
+            likes.Add(model);
+            posts.Where(x => x.Id == postId).FirstOrDefault().PostLikes.Add(model);
+            return posts.Where(x => x.Id == postId).FirstOrDefault();
+        }
+    }
+}
---
+++ LinkedInAPI/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedInAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Likes { get; set; }
+        public string Comments { get; set; }
+        public DateTime? PostDateTime { get; set; }
+        public List<Comment> PostComments { get; set; } = new List<Comment>();
+        public List<Like> PostLikes { get; set; } = new List<Like>(); 
+    }
+    public class Comment
+    {
+        public long Id { get; set; }
+        public long PostId { get; set; }
+        public string Comments { get; set; }
+        public DateTime? CommentDateTime { get; set; }
+    }
+    public class Like
+    {
+        public long Id { get; set; }
+        public long PostId { get; set; }
+        public DateTime? LikeDateTime { get; set; }
+    }
+    
+
+}
